9cf3ef70ad3cbdc65db96dcaf86f3f57aaab8253
cheshta-kabra <64300181+cheshta-kabra@users.noreply.github.com>
2021-01-17 12:30:29 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -6,7 +6,7 @@ class Ball {
           'density':1,
           'frictionAir': 0,
       }
-      function keyPressed() { if (keyCode === UP_ARROW) { Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45}); } } 
+     this.body={{pos.x,pos.y}}
       this.width = width;
 
       World.add(world, this.body);
