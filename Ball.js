5c01338052c006dfdf95ee619c5064ee6d869417
cheshta-kabra <64300181+cheshta-kabra@users.noreply.github.com>
2021-01-17 18:57:45 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -6,10 +6,7 @@ class Ball {
           'density':1,
           'frictionAir': 0,
       }
-     this.x=x;
-     this.y=y;
-     this.width = width;
-     this.body=Bodies.circle(this.x, this.y,this.width,options)
+     this.body=Bodies.circle(x, y,width,options)
 
       World.add(world, this.body);
     }
